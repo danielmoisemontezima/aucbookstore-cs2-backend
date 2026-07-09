3634828213259d3460753515c1af800f172676f7
kensonjanvier6-netizen <kensonjanvier6@gmail.com>
2026-07-09 09:39:07 -0400

    getcatalog

---
+++ src/usecases/src/usecases/getcatalog.py
@@ -0,0 +1,22 @@
+
+from src.entities.furnitures import Furniture
+from src.entities.services import Service
+from src.usecases.interfaces.catalog_repository import CatalogRepository
+
+class GetCatalogue:
+  
+
+    def __init__(self, catalogRepo:CatalogRepository):
+        
+       self.catalogRepo = catalogRepo 
+ #self{attributs ki nan objet a }
+
+    def executeFurnitures(self) -> dict[Furniture]:
+        listFurnitures = self.catalogRepo.getFurniture()
+        return listFurnitures
+    
+    def executeServices(self) -> dict[Services]:
+        listServices = self.catalogRepo.getServices()
+        return listServices
+
+       
